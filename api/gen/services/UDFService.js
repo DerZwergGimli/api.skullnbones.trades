1519df2e2574c9da6c990cc5102ef2911531b7e3
dzwg <paule22@protonmail.com>
2022-09-11 01:19:04 +0200

    commit2

--- api/gen/services/UDFService.js
+++ api/gen/services/UDFService.js
@@ -1,161 +1,194 @@
 /* eslint-disable no-unused-vars */
-const Service = require('./Service');
+const Service = require("./Service");
 
 /**
-* history object
-* Gets a symbol info message 
-*
-* symbol String trading pair symbol (optional)
-* from BigDecimal timestamp from (optional)
-* to BigDecimal timestamp to (optional)
-* resolution String time resolution (optional)
-* countback BigDecimal counter (optional)
-* currencyCode String symbol-cde (optional)
-* returns HistoryElement
-* */
-const geHistoryElement = ({ symbol, from, to, resolution, countback, currencyCode }) => new Promise(
-  async (resolve, reject) => {
+ * history object
+ * Gets a symbol info message
+ *
+ * symbol String trading pair symbol (optional)
+ * from BigDecimal timestamp from (optional)
+ * to BigDecimal timestamp to (optional)
+ * resolution String time resolution (optional)
+ * countback BigDecimal counter (optional)
+ * currencyCode String symbol-cde (optional)
+ * returns HistoryElement
+ * */
+const geHistoryElement = ({
+  symbol,
+  from,
+  to,
+  resolution,
+  countback,
+  currencyCode,
+}) =>
+  new Promise(async (resolve, reject) => {
     try {
-      resolve(Service.successResponse({
-        symbol,
-        from,
-        to,
-        resolution,
-        countback,
-        currencyCode,
-      }));
+      resolve(
+        Service.successResponse({
+          symbol,
+          from,
+          to,
+          resolution,
+          countback,
+          currencyCode,
+        })
+      );
     } catch (e) {
-      reject(Service.rejectResponse(
-        e.message || 'Invalid input',
-        e.status || 405,
-      ));
+      reject(
+        Service.rejectResponse(e.message || "Invalid input", e.status || 405)
+      );
     }
-  },
-);
+  });
 /**
-* symbol into
-* Gets a symbol info message 
-*
-* query String trading pair symbol (optional)
-* limit String limit results (optional)
-* type String type (optional)
-* exchange String exchange (optional)
-* returns List
-* */
-const geSearchSymbol = ({ query, limit, type, exchange }) => new Promise(
-  async (resolve, reject) => {
+ * symbol into
+ * Gets a symbol info message
+ *
+ * query String trading pair symbol (optional)
+ * limit String limit results (optional)
+ * type String type (optional)
+ * exchange String exchange (optional)
+ * returns List
+ * */
+const geSearchSymbol = ({ query, limit, type, exchange }) =>
+  new Promise(async (resolve, reject) => {
     try {
-      resolve(Service.successResponse({
-        query,
-        limit,
-        type,
-        exchange,
-      }));
+      resolve(
+        Service.successResponse({
+          query,
+          limit,
+          type,
+          exchange,
+        })
+      );
     } catch (e) {
-      reject(Service.rejectResponse(
-        e.message || 'Invalid input',
-        e.status || 405,
-      ));
+      reject(
+        Service.rejectResponse(e.message || "Invalid input", e.status || 405)
+      );
     }
-  },
-);
+  });
 /**
-* symbol into
-* Gets a symbol info message 
-*
-* symbol String trading pair symbol (optional)
-* returns Symbol
-* */
-const geSymbol = ({ symbol }) => new Promise(
-  async (resolve, reject) => {
+ * symbol into
+ * Gets a symbol info message
+ *
+ * symbol String trading pair symbol (optional)
+ * returns Symbol
+ * */
+const geSymbol = ({ symbol }) =>
+  new Promise(async (resolve, reject) => {
     try {
-      resolve(Service.successResponse({
-        symbol,
-      }));
+      resolve(
+        Service.successResponse({
+          symbol,
+        })
+      );
     } catch (e) {
-      reject(Service.rejectResponse(
-        e.message || 'Invalid input',
-        e.status || 405,
-      ));
+      reject(
+        Service.rejectResponse(e.message || "Invalid input", e.status || 405)
+      );
     }
-  },
-);
+  });
 /**
-* symbol into
-* Gets a symbols info message 
-*
-* returns SymbolInfo
-* */
-const geSymbolInfo = () => new Promise(
-  async (resolve, reject) => {
+ * symbol into
+ * Gets a symbols info message
+ *
+ * returns SymbolInfo
+ * */
+const geSymbolInfo = () =>
+  new Promise(async (resolve, reject) => {
     try {
-      resolve(Service.successResponse({
-      }));
+      resolve(Service.successResponse({}));
     } catch (e) {
-      reject(Service.rejectResponse(
-        e.message || 'Invalid input',
-        e.status || 405,
-      ));
+      reject(
+        Service.rejectResponse(e.message || "Invalid input", e.status || 405)
+      );
     }
-  },
-);
+  });
 /**
-* src
-* Gets a config message 
-*
-* returns Config
-* */
-const getConfig = () => new Promise(
-  async (resolve, reject) => {
+ * src
+ * Gets a config message
+ *
+ * returns Config
+ * */
+const getConfig = () =>
+  new Promise(async (resolve, reject) => {
     try {
-      resolve(Service.successResponse({
-      }));
+      resolve(
+        Service.successResponse({
+          exchanges: [
+            {
+              value: "BINANCE",
+              name: "Binance",
+              desc: "Binance Exchange",
+            },
+          ],
+          symbols_types: [
+            {
+              value: "crypto",
+              name: "Cryptocurrency",
+            },
+          ],
+          supported_resolutions: [
+            "1",
+            "3",
+            "5",
+            "15",
+            "30",
+            "60",
+            "120",
+            "240",
+            "360",
+            "480",
+            "720",
+            "1D",
+            "3D",
+            "1W",
+            "1M",
+          ],
+          supports_search: true,
+          supports_group_request: false,
+          supports_marks: false,
+          supports_timescale_marks: false,
+          supports_time: true,
+        })
+      );
     } catch (e) {
-      reject(Service.rejectResponse(
-        e.message || 'Invalid input',
-        e.status || 405,
-      ));
+      reject(
+        Service.rejectResponse(e.message || "Invalid input", e.status || 405)
+      );
     }
-  },
-);
+  });
 /**
-* status
-* Gets a status message 
-*
-* returns String
-* */
-const getStatus = () => new Promise(
-  async (resolve, reject) => {
+ * status
+ * Gets a status message
+ *
+ * returns String
+ * */
+const getStatus = () =>
+  new Promise(async (resolve, reject) => {
     try {
-      resolve(Service.successResponse({
-      }));
+      resolve(Service.successResponse({}));
     } catch (e) {
-      reject(Service.rejectResponse(
-        e.message || 'Invalid input',
-        e.status || 405,
-      ));
+      reject(
+        Service.rejectResponse(e.message || "Invalid input", e.status || 405)
+      );
     }
-  },
-);
+  });
 /**
-* time
-* Gets server time 
-*
-* returns BigDecimal
-* */
-const getTime = () => new Promise(
-  async (resolve, reject) => {
+ * time
+ * Gets server time
+ *
+ * returns BigDecimal
+ * */
+const getTime = () =>
+  new Promise(async (resolve, reject) => {
     try {
-      resolve(Service.successResponse({
-      }));
+      resolve(Service.successResponse({}));
     } catch (e) {
-      reject(Service.rejectResponse(
-        e.message || 'Invalid input',
-        e.status || 405,
-      ));
+      reject(
+        Service.rejectResponse(e.message || "Invalid input", e.status || 405)
+      );
     }
-  },
-);
+  });
 
 module.exports = {
   geHistoryElement,
